2cf675295c8012aa72ae0ca9c1727592f2ae925d
Robin Rennesund Ellingsen <46957821+robinElli@users.noreply.github.com>
2021-11-25 15:02:54 +0100

    get templates + disptaches skrevet om

--- func-getdispatches/index.js
+++ func-getdispatches/index.js
@@ -1,25 +1,29 @@
 const mongoose = require("mongoose");
 const Dispatches = require('../sharedcode/models/dispatches.js')
 const getDb = require('../sharedcode/connections/masseutsendelseDB.js')
+const utils = require('@vtfk/utilities');
 
 module.exports = async function (context) {
-    let loadDatabase = await getDb()
-    if (await loadDatabase) {
-        context.log("Mongoose is connected.");
-        let data
-        try {
-            await Dispatches.find({}).then((dispatches) => {
-                data = dispatches
-            })
-        } catch (err) {
-            context.log.error('ERROR', err)
-            throw err
-        }
-        context.res = {
-            status: 200,
-            body: data
-        }
+    try {
+        // Await the DB connection 
+        await getDb()
+        context.log("Mongoose is connected")
+
+        // Find all disptaches 
+        let dispatch = await Dispatches.find({})
+        if(!dispatch) { throw new Error('No dispatches found in the database.') }
+        context.log('== Disptaches ==')
+        utils.inspect(dispatch)
+
+        // Return the disptaches
+        context.res.send(dispatch)
+
+        // Close the database connection 
         mongoose.connection.close()
+    } catch (err) {
+        context.log(err)
+        context.res.status(400).send(err)
+        throw err
     }
 }
 
--- func-gettemplates/index.js
+++ func-gettemplates/index.js
@@ -1,25 +1,29 @@
 const mongoose = require("mongoose");
 const Templates = require('../sharedcode/models/templates.js')
 const getDb = require('../sharedcode/connections/masseutsendelseDB.js')
+const utils = require('@vtfk/utilities');
 
 module.exports = async function (context) {
-    let loadDatabase = await getDb()
-    if (await loadDatabase) {
-        context.log("Mongoose is connected.");
-        let data
-        try {
-            await Templates.find({}).then((templates) => {
-                data = templates
-            })
-        } catch (err) {
-            context.log.error('ERROR', err)
-            throw err
-        }
-        context.res = {
-            status: 200,
-            body: data
-        }
+    try {
+        // Await the db connection 
+        await getDb()
+        context.log("Mongoose is connected")
+
+        // Find all the templates
+        let templates = await Templates.find({})
+        if(!templates) { throw new Error('No templates found in the databases') }
+        context.log('== Templates ==')
+        utils.inspect(templates)
+
+        //Return the Templates
+        context.res.send(templates)
+
+        // Close the database connection 
         mongoose.connection.close()
+    } catch (err) {
+        context.log(err)
+        context.res.status(400).send(err)
+        throw err
     }
 }
 
