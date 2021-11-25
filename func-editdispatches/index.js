a6507a6240d58f1d8885b61991dd63bd19baa234
Robin Rennesund Ellingsen <46957821+robinElli@users.noreply.github.com>
2021-11-25 14:46:23 +0100

    edit dispatches er skrevet om

--- func-editdispatches/index.js
+++ func-editdispatches/index.js
@@ -1,27 +1,41 @@
 const mongoose = require("mongoose");
 const Dispatches = require('../sharedcode/models/dispatches.js')
 const getDb = require('../sharedcode/connections/masseutsendelseDB.js');
-const util = require('util');
+const utils = require('@vtfk/utilities');
 
 module.exports = async function (context, req, id) {
-    var urlId = context.bindingData.id
-    id = `${urlId}`
-    let loadDatabase = await getDb()
-    if (await loadDatabase) {
-        context.log("Mongoose is connected.");
-        try {
-            await Dispatches.findByIdAndUpdate(id, req.body, {new: true}).then((editedDispatch) => {
-                if(!editedDispatch){
-                    context.res.status(404).send()
-                }
-                context.res.send(editedDispatch)
-            }).catch(error => {
-                res.status(500).send(error)
-            })
-        } catch (err) {
-            context.log.error('ERROR', err)
-            throw err
-        } 
+    try {
+        // Get the ID from the request 
+        const id = context.bindingData.id
+
+        // Await the Db conection 
+        await getDb()
+        context.log("Mongoose is connected")
+
+        // Get the existing disptach object 
+        let existingDispatch = await Dispatches.findById(id).lean()
+        if(!existingDispatch) { throw new Error(`Dispatch with id ${id} could not be found` ) }
+        context.log('== Existing Dispatch ==')
+        utils.inspect(existingDispatch)
+
+        // Strip away some fields that should not be set by the request
+        req.body = utils.removeKeys(req.body, ['createdTimestamp', 'createdBy', 'modifiedTimestamp', 'modifiedBy']);
+
+        // Update default values 
+        req.modifiedTimestamp = new Date()
+        // TODO - Oppdater modifiedBy 
+
+        // Update the dispatch 
+        const updatedDispatch = await Dispatches.findByIdAndUpdate(id, req.body, {new: true})
+
+        // Return the dispatch 
+        context.res.status(200).send(updatedDispatch)
+
+        // Close the connection 
+        mongoose.connection.close()
+    } catch (err) {
+        context.log(err)
+        context.res.status(400).send(err)
+        throw err
     }
-    mongoose.connection.close()
 }
