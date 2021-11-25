29fbda2dfed625440b74cc79cce3786d6d5d22df
Robin Rennesund Ellingsen <46957821+robinElli@users.noreply.github.com>
2021-11-25 13:33:37 +0100

    Get templates by id er skrevet om

--- func-gettemplatesbyid/index.js
+++ func-gettemplatesbyid/index.js
@@ -1,26 +1,33 @@
 const mongoose = require("mongoose");
 const Templates = require('../sharedcode/models/templates.js')
 const getDb = require('../sharedcode/connections/masseutsendelseDB.js')
+const utils = require('@vtfk/utilities');
+
 
 module.exports = async function (context, req, id) {
-    var urlId = context.bindingData.id
-    id = `${urlId}`
-    let loadDatabase = await getDb()
-    if (await loadDatabase) {
+    try {
+        // Get ID from request
+        const id = context.bindingData.id
+
+        // Await the database
+        await getDb()
         context.log("Mongoose is connected.");
-        try {
-            await Templates.findById(id, req.body, {new: true}).then((templateObject) => {
-                if(!templateObject){
-                    context.res.status(404).send()
-                }
-                context.res.send(templateObject)
-            }).catch(error => {
-                res.status(500).send(error)
-            })
-        } catch (err) {
-            context.log.error('ERROR', err)
-            throw err
-        } 
+
+        //Find Template by ID
+        let template = await Templates.findById(id)
+        if(!template) { throw new Error(`Template with id ${id} could no be found`) }
+        context.log('== Template ==');
+        utils.inspect(template);
+
+        //Return the template object 
+        let templateById = await Templates.findById(id, req.body, {new: true})
+        context.res.send(templateById)
+
+        // Close the database connection
+        mongoose.connection.close();
+    }catch (err) {
+        context.log(err);
+        context.res.status(400).send(err);
+        throw err;
     }
-    mongoose.connection.close()
 }
